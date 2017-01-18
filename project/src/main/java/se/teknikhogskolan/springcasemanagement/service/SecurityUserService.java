93033f2c38fc13f600c2f9ca938dd3e11c3da9ec
peferb <pe.ferb@gmail.com>
2017-01-18 12:58:26 +0100

    Stopped entity leak from service, can check if username is available, can delete user, can verify token

--- project/src/main/java/se/teknikhogskolan/springcasemanagement/service/SecurityUserService.java
+++ project/src/main/java/se/teknikhogskolan/springcasemanagement/service/SecurityUserService.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.teknikhogskolan.springcasemanagement.model.SecurityUser;
 import se.teknikhogskolan.springcasemanagement.repository.SecurityUserRepository;
+import se.teknikhogskolan.springcasemanagement.service.exception.NotAllowedException;
 import se.teknikhogskolan.springcasemanagement.service.exception.NotAuthorizedException;
 import se.teknikhogskolan.springcasemanagement.service.exception.NotFoundException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import static se.teknikhogskolan.springcasemanagement.service.SecurityHelper.generateSalt;
 import static se.teknikhogskolan.springcasemanagement.service.SecurityHelper.generateToken;
@@ -19,18 +21,18 @@ import static se.teknikhogskolan.springcasemanagement.service.SecurityHelper.has
 @Service
 public class SecurityUserService {
 
-    private  final SecurityUserRepository securityUserRepository;
+    private  final SecurityUserRepository repository;
 
     @Autowired
     public SecurityUserService(SecurityUserRepository securityUserRepository) {
-        this.securityUserRepository = securityUserRepository;
+        this.repository = securityUserRepository;
     }
 
     /** @return id */
     public Long create(String username, String password) throws IllegalArgumentException {
         if (null == username) throw new IllegalArgumentException("Username must not be null");
         if (null == password) throw new IllegalArgumentException("Password must not be null");
-        if (null != securityUserRepository.findByUsername(username)) throw new IllegalArgumentException(String.format(
+        if (null != repository.findByUsername(username)) throw new IllegalArgumentException(String.format(
                 "Username '%s' already exist", username));
 
         String salt = generateSalt();
@@ -38,7 +40,7 @@ public class SecurityUserService {
         Map<String, String> tokens = new HashMap<>();
         tokens.put(generateToken(254), LocalDateTime.now().plusDays(1L).toString());
 
-        SecurityUser user = securityUserRepository.save(new SecurityUser(username, tokens, hashedPassword, salt, hashingIterations));
+        SecurityUser user = repository.save(new SecurityUser(username, tokens, hashedPassword, salt, hashingIterations));
 
         return user.getId();
     }
@@ -49,14 +51,14 @@ public class SecurityUserService {
         if (passwordMatchesUser(password, user)) {
             String token = generateToken(255);
             user.addToken(token, LocalDateTime.now().plusDays(1));
-            securityUserRepository.save(user);
+            repository.save(user);
             return token;
         } else throw new NotAuthorizedException("Wrong password");
     }
 
-    public SecurityUser getByUsername(String username) {
+    private SecurityUser getByUsername(String username) {
 
-        Optional<SecurityUser> user = Optional.ofNullable(securityUserRepository.findByUsername(username));
+        Optional<SecurityUser> user = Optional.ofNullable(repository.findByUsername(username));
         if (!user.isPresent()) throw new NotFoundException(String.format("No such User '%s'", username));
 
         return removeExpiredTokens(user.get());
@@ -73,29 +75,43 @@ public class SecurityUserService {
         user.getTokensExpiration().forEach((token, expirationDate) -> {
             if (now.isAfter(LocalDateTime.parse(expirationDate))) user.getTokensExpiration().remove(token);
         });
-        return securityUserRepository.save(user);
+        return repository.save(user);
     }
 
     private boolean equalPasswords(String password, String salt, String hashedPassword) {
         return hashedPassword.equals(hashPassword(password, salt));
     }
 
-    public SecurityUser getById(Long id) {
-
-        Optional<SecurityUser> user = Optional.ofNullable(securityUserRepository.findOne(id));
-        if (!user.isPresent()) throw new NotFoundException(String.format("No such User '%s'", id));
-
-        return removeExpiredTokens(user.get());
+    public Long delete(String username, String password) {
+        SecurityUser user = getByUsername(username);
+        if (passwordMatchesUser(password, user)) {
+            repository.delete(user.getId());
+            return user.getId();
+        } else throw new NotAuthorizedException("Wrong password");
     }
 
-    public SecurityUser delete(Long id) {
-        SecurityUser user = getById(id);
-        securityUserRepository.delete(id);
-        return user;
+    private SecurityUser getByToken(String token) {
+        SecurityUser user = repository.findByToken(token);
+        user = removeExpiredTokens(user);
+        if (user.getTokensExpiration().containsKey(token)) {
+            return user;
+        } else throw new NotAllowedException("Token expired");
     }
 
-    public SecurityUser getByToken(String token) {
-        return securityUserRepository.findByToken(token);
+    public boolean verify(String username, String token) {
+        SecurityUser user = getByToken(token);
+        if (username.equals(user.getUsername())) {
+            return true;
+        } else return false;
     }
 
+    public boolean usernameIsAvailable(String username) {
+        return null == repository.findByUsername(username);
+    }
+
+    @Deprecated
+    public LocalDateTime getExpiration(String token) throws NotImplementedException{
+        throw new NotImplementedException();
+//        return LocalDateTime.parse(repository.getTokenExpiration(token));
+    }
 }
